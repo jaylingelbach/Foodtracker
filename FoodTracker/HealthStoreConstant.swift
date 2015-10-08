80ce5e3a400237de01d9594d8ea49d7b5b35a99f
Jay <jay.lingelbach@gmail.com>
2015-10-08 12:05:01 -0500

    HealthKit auth complete

---
+++ FoodTracker/HealthStoreConstant.swift
@@ -0,0 +1,9 @@
+//
+//  HealthStoreConstant.swift
+//  FoodTracker
+//
+//  Created by Jay Lingelbach on 10/8/15.
+//  Copyright (c) 2015 Jay Lingelbach. All rights reserved.
+//
+
+import Foundation
